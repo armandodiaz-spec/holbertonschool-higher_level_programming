1011b27f727542c0d5507abe8cbf93a233ff5fe1
root <root@ip-10-42-251-88.ec2.internal>
2026-01-30 22:31:16 +0000

    SQL_more_queries: list shows without genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,10 @@
+-- Lists all shows without a genre linked
+-- Displays: tv_shows.title, tv_show_genres.genre_id (NULL)
+-- Sorted by tv_shows.title, tv_show_genres.genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
