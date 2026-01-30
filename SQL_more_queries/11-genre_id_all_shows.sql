1ca4ccdf98ca266bcc9e6f1baa7c7d0c4fd4e14e
root <root@ip-10-42-251-88.ec2.internal>
2026-01-30 22:30:31 +0000

    SQL_more_queries: list genre id for all shows

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- Lists all shows contained in hbtn_0d_tvshows
+-- Displays: tv_shows.title, tv_show_genres.genre_id
+-- If a show doesn't have a genre, display NULL
+-- Sorted by tv_shows.title, tv_show_genres.genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
