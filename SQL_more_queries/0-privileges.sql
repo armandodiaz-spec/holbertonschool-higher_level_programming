544ec6e2157c0968e14f242259285965c0c253ea
root <root@ip-10-42-251-88.ec2.internal>
2026-01-30 20:52:19 +0000

    SQL_more_queries: list privileges for users

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,7 @@
--- List privileges for the MySQL users user_0d_1 and user_0d_2 on localhost
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
+-- Show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
