7dfd6b6c8bd8eab92c96846d119273720fb7f484
armandodiaz-spec <armando.diaz@anahuacmayab.edu.mx>
2026-01-30 20:30:40 +0000

    SQL more queries: list privileges for users

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- List privileges for the MySQL users user_0d_1 and user_0d_2 on localhost
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
