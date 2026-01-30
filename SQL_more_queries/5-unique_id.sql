cdd6a8a7ec57b71d2ace3ce290bdbd3dc272ff95
root <root@ip-10-42-251-88.ec2.internal>
2026-01-30 22:20:20 +0000

    SQL_more_queries: create unique_id table

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- Creates the table unique_id on the current database
+-- id has a default value of 1 and must be unique
+
+CREATE TABLE IF NOT EXISTS unique_id (
+  id INT DEFAULT 1 UNIQUE,
+  name VARCHAR(256)
+);
